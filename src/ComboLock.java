c76843b270fbb3150861804c0ea163aafb31beb4
Tyrell <Tyrell102@gmail.com>
2018-11-28 18:10:51 -0500

    Adding more comments.

--- src/ComboLock.java
+++ src/ComboLock.java
@@ -1,4 +1,4 @@
-public class ComboLock { // Creating ComboLock class
+public class ComboLock { // Creating ComboLock class 
 
     // Private integer fields
     private int combo1, combo2, combo3;
@@ -14,28 +14,28 @@ public class ComboLock { // Creating ComboLock class
         combo2 = c2;
         combo3 = c3;
 
-        if (c1 < 0){
+        if (c1 < 0){ // This will be keep the dial from going to any negative numbers
             combo1 = 0;
         }
-        else if (c1 > 39){
+        else if (c1 > 39){// if c1 = 39 dial will be set to 0 so the dial isn't turned past 39
             combo1 = 39;
         }
         else{
             combo1 = c1;
         }
-        if(c2 < 0){
+        if(c2 < 0){ // This will be keep the dial from going to any negative numbers
             combo2 = 0;
         }
-        else if(c2 > 39){
+        else if(c2 > 39){// if c2 = 39 dial will be set to 0 so the dial isn't turned past 39
             combo2 = 0;
         }
         else{
             combo2 = 0;
         }
-        if (c3 < 0) {
+        if (c3 < 0) {// This will be keep the dial from going to any negative numbers
             combo3 = 0;
         }
-        else if (c3 > 39){
+        else if (c3 > 39){// if c3 = 39 dial will be set to 0 so the dial isn't turned past 39
             combo3 = 39;
         }
         else{
@@ -80,7 +80,7 @@ public class ComboLock { // Creating ComboLock class
 
     }
 
-    public boolean open(){
+    public boolean open(){//This method is will determine if the combination is correct
         if(combo1 == set1 && combo2 == set2 && combo3 == set3){
             return true;
         }
@@ -88,7 +88,7 @@ public class ComboLock { // Creating ComboLock class
             return false;
         }
     }
-    public void reset(){
+    public void reset(){//This method will reset the dial and sets to 0
         dial = 0;
         set1 = 0;
         set2 = 0;
